b84d68f2d84ebe7aabe931ee9393065742721096
never-better <youtaktak@gmail.com>
2022-07-04 15:45:18 +0900

    first commit

---
+++ action/actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  actionApp.swift
+//  action
+//
+//  Created by youtak on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
